8d54d129199237722a276b4baea91b36a3b33020
Gokhan Tatlicilar <gokhantatlicilar@gmail.com>
2018-11-13 16:43:20 +0300

    september_13th_added

--- sms_testing.py
+++ sms_testing.py
@@ -1,8 +1,17 @@
 import serial
-
-port = serial.Serial("/dev/ttyS0", baudrate=9600)
-
-port.write('AT'.encode())
-rcv = port.read(40)
-print(rcv)
-#print(serial.rcv())
+import io
+from array import *
+from codecs import encode #(alttaki ile birbirinin alternatifidir)
+#from binascii import hexlify
+ser = serial.Serial("/dev/ttyS0",9600)
+ser2= serial.serial_for_url('loop://',timeout=0)
+ser.write("AT\n".encode())
+liste=[]
+while ser.open:
+    s =ser.readline()
+    s=(s.decode('utf-8'))
+    s=s[:-2]
+    #if s != 'AT':
+    #liste.append(s)
+    print(s)#liste)
+    
